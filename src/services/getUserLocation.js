b4832ac40a9229f64573c4f65cbfa0116f4b1442
Malberee <82259650+Malberee@users.noreply.github.com>
2023-05-12 22:02:09 +0300

    fix geolocation

--- src/services/getUserLocation.js
+++ src/services/getUserLocation.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+const API_KEY = '8834fdcd896e483ebb4297fb7757e842'
+
 export const getUserLocation = async () => {
 	const userCity = await axios
-		.get('https://ip-api.com/json/?fields=49183')
-        .then((res) => res.data.city)
+		.get(`https://api.geoapify.com/v1/ipinfo?&apiKey=${API_KEY}`)
+		.then((res) => res.data.city.name)
 	return userCity
+
 }
