5beb76b26fb21516827c6d1990a72b24504df5f2
Malberee <82259650+Malberee@users.noreply.github.com>
2023-05-12 21:26:06 +0300

    Update getUserLocation.js

--- src/services/getUserLocation.js
+++ src/services/getUserLocation.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const getUserLocation = async () => {
 	const userCity = await axios
-		.get('http://ip-api.com/json/?fields=49183')
+		.get('https://ip-api.com/json/?fields=49183')
         .then((res) => res.data.city)
 	return userCity
 }
